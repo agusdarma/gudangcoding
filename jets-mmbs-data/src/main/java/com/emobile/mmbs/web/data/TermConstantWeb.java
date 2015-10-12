c43261cb8d096621b20bf6bbbc148745a97cbc15
Agus Darma Kusuma <agusdk2011@gmail.com>
2015-10-12 10:42:36 +0700

    a

--- jets-mmbs-data/src/main/java/com/emobile/mmbs/web/data/TermConstantWeb.java
+++
@@ -1,7 +0,0 @@
-package com.emobile.mmbs.web.data;
-
-public class TermConstantWeb {
-	public static final String PREFIX_PAYMENT 		= "PAYMENT_";
-	public static final String TERM_CUSTOMER_NAME   = "CustomerName";
-	public static final String TERM_PASSENGER_NAME 	= "PassengerName";
-}
--- jets-mmbs-data/src/main/java/com/emobile/mmbs/web/mapper/MiscMapper.java
+++
@@ -1,9 +0,0 @@
-package com.emobile.mmbs.web.mapper;
-
-import java.util.Date;
-
-public interface MiscMapper {
-	
-	public Date selectCurrentTime();
-	
-}
